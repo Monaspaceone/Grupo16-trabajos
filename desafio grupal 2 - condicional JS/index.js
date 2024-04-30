d195ff64dd3827decbeaa7b629ad30f4275a2f2d
Viper2678 <gabrielarnaldoaguero816@gmail.com>
2024-04-30 15:52:41 -0300

    commit 3- sintaxis
    pregunto si la sintaxis esta bien escrita en un comentario

--- desafio grupal 2 - condicional JS/index.js
+++ desafio grupal 2 - condicional JS/index.js
@@ -3,14 +3,14 @@ var password = "2024";
 
 var usuario=prompt("Ingrese usuario","");
 var clave=prompt("Ingrese contraseña","");
-if (user!=usuario || password!=clave)
-{
+if (user!=usuario || password!=clave)//me parece que aca va el operador lógico && donde las 2 
+{                                    //condiciones tienen que ser verdaderas
     document.write("Usuario o clave incorrectos. Intente nuevamente"); 
 } else 
 {
     document.write("Bienvenido ",usuario)
 }
 
-console.log("Datos guardados: ",user," ",password);
+console.log("Datos guardados: ",user," ",password);//y aca no se concatena con un +?
 console.log("Datos ingresados: ",usuario," ",clave);
 
